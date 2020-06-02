fc954757ba40446b51e925414f5dcc23e4985925
arian reyes <arianr@gmail.com>
2020-06-02 18:18:08 -0500

    agregando modelo command al proyecto API

---
+++ src/CommandAPI/Models/Commands.cs
@@ -0,0 +1,11 @@
+namespace CommandAPI.Models{
+    public class Command{
+
+        public int Id {get;set;}
+        public string Howto {get;set;}
+        public string Plataform {get;set;}
+        public string commandLine {get;set;}
+        
+
+    }
+}
