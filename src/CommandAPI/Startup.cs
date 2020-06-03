ef076ab376d1a7db52b93cb9f9f04ba52f282743
arian reyes <arianr@gmail.com>
2020-06-03 12:13:43 -0500

    pasos

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using CommandAPI.Models;
+using Npgsql;
+
 
 namespace CommandAPI
 {
@@ -24,8 +26,13 @@ namespace CommandAPI
         public void ConfigureServices(IServiceCollection services)
         {
 
+            var builder = new NpgsqlConnectionStringBuilder(); 
+            builder.ConnectionString = Configuration.GetConnectionString("PostgreSqlConnection"); 
+            builder.Username = Configuration["UserID"]; 
+            builder.Password = Configuration["Password"];
+
             services.AddDbContext<CommandContext>(opt=> opt.UseNpgsql
-            (Configuration.GetConnectionString("PostgreSqlConnection")));
+            (builder.ConnectionString));
 
             //SECTION 1 
             services.AddControllers();
