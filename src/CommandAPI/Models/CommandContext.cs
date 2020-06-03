e0197003188624d835d18058be79f8b2b94ce27f
arian reyes <arianr@gmail.com>
2020-06-02 20:37:26 -0500

    agregando dbcontext

---
+++ src/CommandAPI/Models/CommandContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+namespace CommandAPI.Models{
+    public class CommandContext : DbContext{
+
+        public CommandContext(DbContextOptions<CommandContext> options):base(options){
+
+        }
+        public DbSet<Command> CommandItems {get;set;}
+
+    }
+}
