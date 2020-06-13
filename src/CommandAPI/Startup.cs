6850c9d97944a99b2215f7fa6bb884e9d2a77c9f
arian reyes <arianr@gmail.com>
2020-06-12 22:03:53 -0500

    Ef azure

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -39,8 +39,10 @@ namespace CommandAPI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
+        CommandContext context)
         {
+            context.Database.Migrate();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
